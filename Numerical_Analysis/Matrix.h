22efa312f891c62b4e5287fce56f27587fa692b4
Michael <kimh060612@naver.com>
2019-12-27 22:58:42 +0900

    .

--- Numerical_Analysis/Matrix.h
+++ Numerical_Analysis/Matrix.h
@@ -1,18 +1,89 @@
 #include <math.h>
 #include <vector>
 
+using namespace std;
+
 class Matrix 
 {
 public:
     int row, col;
-    std::vector<std::vector<int>> MAT;
+    vector<vector<int>> MAT;
 
     Matrix(int R, int C);
+
+    Matrix operator + (Matrix op1);
+    Matrix operator - (Matrix op1);
+    Matrix operator * (Matrix op1);
+
+
 
 };
 
 Matrix::Matrix(int R, int C)
 {
     this->row = R;
-    this->col = C;   
+    this->col = C;
+       
+    this->MAT.resize(R);
+    for (int i = 0; i < R; i++)
+    {
+        this->MAT[i].assign(C,0);
+    }
+
+}
+
+Matrix Matrix::operator+ (Matrix op1)
+{
+    if (this->row != op1.row || this->col != op1.col)
+    {
+        cout << "Error: The shape of matrix doen't match!" << endl;
+        return;
+    }
+
+    Matrix RES(this->row, this->col);
+
+    for (int i = 0; i <= this->row; i++)
+    {
+        for(int j = 0; j <= this->col; j++)
+        {
+            RES.MAT[i][j] = this->MAT[i][j] + op1.MAT[i][j];
+        }
+    }
+
+    return RES;
+}
+
+Matrix Matrix::operator- (Matrix op1)
+{
+    if (this->row != op1.row || this->col != op1.col)
+    {
+        cout << "Error: The shape of matrix doen't match!" << endl;
+        return;
+    }
+
+    Matrix RES(this->row, this->col);
+
+    for (int i = 0; i <= this->row; i++)
+    {
+        for(int j = 0; j <= this->col; j++)
+        {
+            RES.MAT[i][j] = this->MAT[i][j] - op1.MAT[i][j];
+        }
+    }
+    
+    return RES;
+}
+
+Matrix Matrix::operator* (Matrix op1)
+{
+    if (this->row != op1.row || this->col != op1.col)
+    {
+        cout << "Error: The shape of matrix doen't match!" << endl;
+        return;
+    }
+
+    Matrix RES(this->row, this->col);
+
+    
+
 }
--- Numerical_Analysis/test.cpp
+++ Numerical_Analysis/test.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include "Matrix.h"
 
-using namespace std;
-
 int main()
 {
     Matrix A(3,4);
 
     cout << A.col  << " " << A.row << endl;
-
+    
     return 0;
 
 }
