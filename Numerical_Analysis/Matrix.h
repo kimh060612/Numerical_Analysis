57b58afce442c79a97e132b76061a9c39500a278
Michael <kimh060612@naver.com>
2019-12-28 00:05:24 +0900

    .

--- Numerical_Analysis/Matrix.h
+++ Numerical_Analysis/Matrix.h
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-class Matrix 
+class Matrix
 {
 public:
     int row, col;
@@ -13,26 +13,24 @@ public:
     Matrix(int R, int C);
     Matrix(vector<vector<int>> MAT_);
 
-    Matrix operator + (Matrix& op1);
-    Matrix operator - (Matrix& op1);
-    Matrix operator * (Matrix& op1);
-    Matrix operator * (int Constant);
-
+    Matrix operator+(Matrix &op1);
+    Matrix operator-(Matrix &op1);
+    Matrix operator*(Matrix &op1);
+    Matrix operator*(int Constant);
+    
     void show();
-
 };
 
 Matrix::Matrix(int R, int C)
 {
     this->row = R;
     this->col = C;
-       
+
     this->MAT.resize(R);
     for (int i = 0; i < R; i++)
     {
-        this->MAT[i].assign(C,0);
+        this->MAT[i].assign(C, 0);
     }
-
 }
 
 Matrix::Matrix(vector<vector<int>> MAT_)
@@ -46,85 +44,107 @@ Matrix::Matrix(vector<vector<int>> MAT_)
     this->col = this->MAT[0].size();
 }
 
-Matrix Matrix::operator+ (Matrix& op1)
+Matrix Matrix::operator+(Matrix &op1)
 {
-    if (this->row != op1.row || this->col != op1.col)
+    try
     {
-        cout << "Error: The shape of matrix doen't match!" << endl;
-        return;
-    }
-
-    Matrix RES(this->row, this->col);
-
-    for (int i = 0; i <= this->row; i++)
-    {
-        for(int j = 0; j <= this->col; j++)
+        if (this->row != op1.row || this->col != op1.col)
         {
-            RES.MAT[i][j] = this->MAT[i][j] + op1.MAT[i][j];
+            int e = 0;
+            throw e;
         }
-    }
 
-    return RES;
-}
+        Matrix RES(this->row, this->col);
 
-Matrix Matrix::operator- (Matrix& op1)
-{
-    if (this->row != op1.row || this->col != op1.col)
-    {
-        cout << "Error: The shape of matrix doen't match!" << endl;
-        return;
-    }
-
-    Matrix RES(this->row, this->col);
-
-    for (int i = 0; i < this->row; i++)
-    {
-        for(int j = 0; j < this->col; j++)
+        for (int i = 0; i <= this->row; i++)
         {
-            RES.MAT[i][j] = this->MAT[i][j] - op1.MAT[i][j];
-        }
-    }
-    
-    return RES;
-}
-
-Matrix Matrix::operator* (Matrix& op1)
-{
-    if (this->col != op1.row)
-    {
-        cout << "Error: The shape of matrix doen't match!" << endl;
-        return;
-    }
-
-    Matrix RES(this->row, op1.col);
-
-    for(int i = 0; i < this->row; i++)
-    {
-        for(int j = 0; j < op1.col; j++)
-        {
-            for(int k = 0; k < this->col; k++)
+            for (int j = 0; j <= this->col; j++)
             {
-                RES.MAT[i][j] += this->MAT[i][k]*op1.MAT[k][j];
+                RES.MAT[i][j] = this->MAT[i][j] + op1.MAT[i][j];
             }
         }
-    }
 
-    return RES;
+        return RES;
+    }
+    catch (const int e)
+    {
+        cout << "Error: The shape of matrix doen't match!" << endl;
+    }
 }
 
-Matrix Matrix::operator * (int Constant)
+Matrix Matrix::operator-(Matrix &op1)
+{
+    try
+    {
+        if (this->row != op1.row || this->col != op1.col)
+        {
+            int e = 0;
+            throw e;
+        }
+
+        Matrix RES(this->row, this->col);
+
+        for (int i = 0; i <= this->row; i++)
+        {
+            for (int j = 0; j <= this->col; j++)
+            {
+                RES.MAT[i][j] = this->MAT[i][j] - op1.MAT[i][j];
+            }
+        }
+
+        return RES;
+    }
+    catch (const int e)
+    {
+        cout << "Error: The shape of matrix doen't match!" << endl;
+    }
+}
+
+Matrix Matrix::operator*(Matrix &op1)
+{
+
+    try
+    {
+        if (this->col != op1.row)
+        {
+            int e = 0;
+            throw e;
+        }
+
+        Matrix RES(this->row, op1.col);
+
+        for (int i = 0; i < this->row; i++)
+        {
+            for (int j = 0; j < op1.col; j++)
+            {
+                for (int k = 0; k < this->col; k++)
+                {
+                    RES.MAT[i][j] += this->MAT[i][k] * op1.MAT[k][j];
+                }
+            }
+        }
+
+        return RES;
+    }
+    catch (const std::exception &e)
+    {
+        cout << "Error: The shape of matrix doen't match!" << endl;
+    }
+}
+
+Matrix Matrix::operator*(int Constant)
 {
 
     Matrix RES(this->row, this->col);
 
     for (int i = 0; i < this->row; i++)
     {
-        for(int j = 0; j < this->col; j++)
+        for (int j = 0; j < this->col; j++)
         {
-            RES.MAT[i][j] = Constant*this->MAT[i][j];
+            RES.MAT[i][j] = Constant * this->MAT[i][j];
         }
     }
-    
+
     return RES;
 }
 
