12a11151953baaa490b81aa1cdf07aa7270a7b07
kimh060612 <kimh060612@naver.com>
2020-12-24 22:27:45 +0900

    .

---
+++ NumericalAnalysisBible/Chap2/Gaussian_Elimination_with_Scaled_Partial_Pivoting/GaussElimination.cpp
@@ -0,0 +1,54 @@
+/*
+Gaussian Elimination With Scaled Partial Pivoting
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <math.h>
+
+using namespace std;
+
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+int main()
+{
+    int N;
+    fin >> N;
+    double **A = new double*[N];
+    double *X = new double[N];
+    double *B = new double[N];
+    for (int i = 0; i < N; i++)A[i] = new double[N];
+    for (int i = 0; i < N; i++)X[i] = 0.;
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            fin >> A[i][j];
+    for (int i = 0; i < N; i++)fin >> B[i];
+
+    
+
+    X[N - 1] = B[N - 1]/A[N - 1][N - 1];
+    for (int i = N - 2; i >= 0; i--)
+    {
+        double sum = B[i];
+        for (int j = i+1; j < N; j++)
+        {
+            sum = sum - A[i][j]*X[j];
+        }
+        X[i] = sum/A[i][i];
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        fout << X[i] << setw(10);
+    }
+
+    for (int i = 0; i < N; i++) delete A[i];
+    delete A;
+    delete B;
+    delete X;
+    fin.close();
+    fout.close();
+    return 0;
+}
--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Numerical_Analysis_study
 
 ### Matrix, Calculus, Statistics, Machine learning, etc... 
 
-> various range of  mathematical library 
+> Numerical Mathematics and computing & Neural Network Design Practice
 > And various mathematical computation for machine learning.
