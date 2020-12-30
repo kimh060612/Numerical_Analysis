269c89a460e29bb254a7c4ca49d8f3907bce439b
kimh060612 <kimh060612@naver.com>
2020-12-30 15:30:09 +0900

    .

---
+++ NumericalAnalysisBible/Chap3/bisection_method/false_position_method.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+double F(double x);
+
+int main()
+{
+    int iter = 1000, epoch = 0;
+    double eps = 0.0001;
+    double a, b, c;
+    a = 0.5;
+    b = 2.;
+    double error = b - a;
+    double fa = F(a) , fb = F(b);
+    while (abs(error) > eps && epoch < iter)
+    {
+        error = b - a;
+        c = (a * fb - b*fa)/(fb - fa);
+        double fc = F(c);
+        if (fa*fc < 0)
+        {
+            b = c;
+            fb = fc;
+        }
+        else
+        {
+            a = c;
+            fa = fc;
+        }
+        epoch ++;
+    }
+
+    cout << c << " " <<error << endl;
+
+    return 0;
+}
+
+double F(double x)
+{
+    return x*x*x - 2*sin(x);
+}
